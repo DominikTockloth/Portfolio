6b53cc1ac798159b7bc37f25d7f63c9ce6b1d89b
DominikTockloth <128898734+DominikTockloth@users.noreply.github.com>
2024-02-15 09:23:19 +0100

    added contact form

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule, NgIf } from '@angular/common';
-import { Component, ElementRef, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, ElementRef, inject } from '@angular/core';
 import { FormGroup, FormBuilder, Validators, ReactiveFormsModule } from '@angular/forms';
 import { TranslateModule } from '@ngx-translate/core';
 
@@ -10,7 +11,7 @@ import { TranslateModule } from '@ngx-translate/core';
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [CommonModule, NgIf, ReactiveFormsModule , TranslateModule],
+  imports: [CommonModule, NgIf, ReactiveFormsModule, TranslateModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
@@ -18,15 +19,57 @@ import { TranslateModule } from '@ngx-translate/core';
 export class ContactComponent {
   contactForm: FormGroup;
   isSubmitted = false;
+  mailTest = true;
 
+  http = inject(HttpClient);
 
-  constructor(private foBu: FormBuilder, private elementRef: ElementRef ) {
+  constructor(private foBu: FormBuilder, private elementRef: ElementRef) {
 
     this.contactForm = this.foBu.group({
       name: ['', Validators.required],
       email: ['', [Validators.required, Validators.email]],
       message: ['', Validators.required]
     });
+  }
+
+  post = {
+    endPoint: 'https://dominik-tockloth.de/sendMail.php',
+    body: (payload: any) => JSON.stringify(payload),
+    options: {
+      headers: {
+        'Content-Type': 'text/plain',
+        responseType: 'text',
+      },
+    },
+  };
+
+
+
+
+  onSubmit() {
+    if (this.contactForm.valid && !this.isSubmitted) {
+      // Formular ist gültig, hier können Sie die Sendelogik implementieren
+      console.log('Formular ist gültig, sende Nachricht:', this.contactForm);
+      this.http.post(this.post.endPoint, this.post.body(this.contactForm))
+        .subscribe({
+          next: (response) => {
+            this.clearForm();
+          },
+          error: (error) => {
+            console.error(error);
+          },
+          complete: () => console.info('send post complete'),
+        });
+
+    } else {
+      // Markieren Sie die Formularfelder, die nicht gültig sind
+      Object.keys(this.contactForm.controls).forEach(field => {
+        const control = this.contactForm.get(field);
+        if (control?.invalid) {
+          control.markAsTouched({ onlySelf: true });
+        }
+      });
+    }
   }
 
   ngOnInit(): void {
@@ -41,32 +84,5 @@ export class ContactComponent {
     this.contactForm.reset();
   };
 
-
-  onSubmit() {
-    if (this.contactForm.valid) {
-      this.isSubmitted = true;
-      // Formular ist gültig, hier können Sie die Sendelogik implementieren
-      console.log('Formular ist gültig, sende Nachricht:', this.contactForm);
-      setTimeout(() => {
-        this.clearForm();
-      }, 3000)
-    } else {
-      // Markieren Sie die Formularfelder, die nicht gültig sind
-      Object.keys(this.contactForm.controls).forEach(field => {
-        const control = this.contactForm.get(field);
-        if (control?.invalid) {
-          control.markAsTouched({ onlySelf: true });
-        }
-      });
-    }
-  }
 }
 
-/*
-  onSubmit(ngForm: NgForm) {
-    if (ngForm.valid && ngForm.submitted) {
-      console.log(this.contactData);
-    }
-  }
-}
-*/
