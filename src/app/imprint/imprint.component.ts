3c78847ebe47d8a361a58c3efad6ac515bce9f5c
DominikTockloth <128898734+DominikTockloth@users.noreply.github.com>
2024-02-19 13:21:11 +0100

    added imprint trnslation

--- src/app/imprint/imprint.component.ts
+++ src/app/imprint/imprint.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { FooterComponent } from '../footer/footer.component';
-
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-imprint',
   standalone: true,
-  imports: [NavbarComponent , FooterComponent],
+  imports: [NavbarComponent, FooterComponent, TranslateModule],
   templateUrl: './imprint.component.html',
   styleUrl: './imprint.component.scss'
 })
